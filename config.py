fb7053d75657c16805af11067f4c3a96565db08b
kris <158147622+krishna3251@users.noreply.github.com>
2025-04-15 13:49:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("AIzaSyBEB9dwPGt6b0OHBK5BTlx88nSEqKffTT0")
-GOOGLE_CSE_ID = os.getenv("b22a1caf922df4ee3")
 # Get the bot token from .env
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
